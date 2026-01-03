b506a0e808b2be0649309962c61ad15ee9fcd140
aiavenak <aiavenak@indiana.edu>
2026-01-02 19:35:53 -0500

    updates to step 3: mostly to make comparison between metadata csv and inventory csv easier

--- s3_organize/create_dicom_dir_csv.sh
+++ s3_organize/create_dicom_dir_csv.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-cd /path/to/unzipped/dicom/directories/
+#raw_dicom_dir="/N/project/cfn-rady/andrea-dev/ADNI/LONI_data/image_data/ADNI"
+#raw_dicom_dir="/N/project/ADNI_rawdata/neuroimaging/sourcedata/original/participants"
 
-echo “sub_ID, scan_name,scan_date” >> dicom_dirs.csv
-for i in *
-do 
-  for j in `ls ${i}/`
-  do 
-    for k in `ls ${i}/${j}/`
-    do 
-      for l in `ls ${i}/${j}/${k}/` 
-      do 
-      echo “${i},${j},${k},${l}” >> dicom_dirs.csv
+raw_dicom_dir=$1  # Pass the raw DICOM directory as the first argument
+
+if [ -z "$raw_dicom_dir" ]; then
+  echo "Usage: $0 /path/to/raw_dicom_directory"
+  exit 1
+fi
+
+if [ ! -d "$raw_dicom_dir" ]; then
+  echo "Error: Directory $raw_dicom_dir does not exist."
+  exit 1
+fi
+
+cd ${raw_dicom_dir}
+
+echo "Subject,Description,Acq Date,ImageID" >> unzipped_dicom_dirs_inventory.csv
+
+for sub in *; do
+  [[ -d "$sub" ]] || continue
+  echo "Processing subject: ${sub}"
+
+  for series in "$sub"/*; do
+    [[ -d "$series" ]] || continue
+    series_name=$(basename "$series")
+    clean_series=${series_name//,/--}  ## found Series Names with comas and that breaks the CSV files. 
+
+    for date in "$series"/*; do
+      [[ -d "$date" ]] || continue
+      date_name=$(basename "$date")
+
+      for imgID in "$date"/*; do
+        [[ -d "$imgID" ]] || continue
+        imgID_name=$(basename "$imgID")
+
+        echo "${sub},${clean_series},${date_name},${imgID_name}" >> unzipped_dicom_dirs_inventory.csv
       done
     done
   done
 done
+
+echo "CSV file created: unzipped_dicom_dirs_inventory.csv"
