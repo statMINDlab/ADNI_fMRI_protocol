e2ec72417621a105d1a25f04df6f1b4860ec1514
aiavenak <aiavenak@indiana.edu>
2026-01-12 16:07:36 -0500

    modifacations made to get code to work for me

--- s4_clinica/adni_submit_slurm.sh
+++ s4_clinica/adni_submit_slurm.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+path2slurmScripts=$1  # path to dir where SLURM scripts will be created
+path2subjectList=$2  # path to text file with list of subject IDs
+modality=$3  # imaging modality: anat, func, dti
 
-for i in `cat adni_subs.txt`
-do
-    sbatch ${i}_adni_clinica.slurm
+#check that paths exist
+if [ ! -d ${path2slurmScripts} ]; then
+    echo "Error: Path to SLURM scripts directory ${path2slurmScripts} does not exist."
+    exit 1
+fi
+
+if [ ! -f ${path2subjectList} ]; then
+    echo "Error: Path to subject list file ${path2subjectList} does not exist."
+    exit 1
+fi 
+
+#check that modality is one of func, anat, dti
+if [ "${modality}" != "func" ] && [ "${modality}" != "anat" ] && [ "${modality}" != "dti" ]; then
+    echo "Error: Modality ${modality} is not recognized. Please use 'func', 'anat', or 'dti'."
+    exit 1
+fi
+
+for i in `cat ${path2subjectList}`; do
+    echo ${i}
+    if [ ! -f ${path2slurmScripts}/${i}_adni_clinica_${modality}.slurm ]; then
+        echo "Error: ${path2slurmScripts}/${i}_adni_clinica_${modality}.slurm. NOT FOUND."
+        continue
+    fi
+    sbatch ${path2slurmScripts}/${i}_adni_clinica_${modality}.slurm
 done
--- s4_clinica/create_slurm_script_per_sub.sh
+++ s4_clinica/create_slurm_script_per_sub.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 
-cd /N/project/statadni/Scripts/Clinica/
+path2slurmScripts=$1   # path to dir where SLURM scripts will be created
+path2subjectList=$2   # path to text file with list of subject IDs
 
-for i in `cat adni_subs.txt`
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+path2slurmTemplate="${3:-$script_dir}"
+
+[[ -d "$path2slurmTemplate" || -f "$path2slurmTemplate" ]] \
+  || { echo "Invalid path2slurmTemplate: $path2slurmTemplate" >&2; exit 1; }
+
+
+#check that paths exist
+if [ ! -d ${path2slurmScripts} ]; then
+    echo "Error: Path to SLURM scripts directory ${path2slurmScripts} does not exist."
+    exit 1
+fi
+
+if [ ! -f ${path2subjectList} ]; then
+    echo "Error: Path to subject list file ${path2subjectList} does not exist."
+    exit 1
+fi  
+
+# #check that modality is one of func, anat, dti
+# if [ "${modality}" != "func" ] && [ "${modality}" != "anat" ] && [ "${modality}" != "dti" ]; then
+#     echo "Error: Modality ${modality} is not recognized. Please use 'func', 'anat', or 'dti'."
+#     exit 1
+# fi
+
+#cd ${path2slurmScripts}
+
+for i in `cat ${path2subjectList}`
 do
     echo ${i}
-    echo ${i} >> ${i}.txt
-    cp adni_clinica.slurm ./${i}_adni_clinica.slurm
-    sed -i "s|job-name=ADNI|job-name=ADNI_${i}|" ${i}_adni_clinica.slurm
-    sed -i "s|cl-ADNI|cl-ADNI_${i}|" ${i}_adni_clinica.slurm
-    sed -i "s|adni_subs|${i}|" ${i}_adni_clinica.slurm
-    sed -i "s|adni_clinica_log|adni_${i}_clinica_log|" ${i}_adni_clinica.slurm
+    echo ${i} > ${path2slurmScripts}/${i}.txt
+    for m in func anat dti; do
+        cp ${path2slurmTemplate}/adni_clinica_${m}.slurm ${path2slurmScripts}/${i}_adni_clinica_${m}.slurm
+        sed -i "s|job-name=adni2bids|job-name=${i}_${m}|" ${path2slurmScripts}/${i}_adni_clinica_${m}.slurm
+        sed -i "s|ADNI-subID|${i}|" ${path2slurmScripts}/${i}_adni_clinica_${m}.slurm
+        sed -i "s|sub2convert|${i}|" ${path2slurmScripts}/${i}_adni_clinica_${m}.slurm
+        sed -i "s|adni_clinica_log|${i}_${m}_log|" ${path2slurmScripts}/${i}_adni_clinica_${m}.slurm
+    done
+
+    # cp ${path2slurmTemplate}/adni_clinica.slurm ${path2slurmScripts}/${i}_adni_clinica.slurm
+    # sed -i "s|job-name=adni2bids|job-name=adni2bids_${i}|" ${path2slurmScripts}/${i}_adni_clinica.slurm
+    # sed -i "s|ADNI-subID|${i}|" ${path2slurmScripts}/${i}_adni_clinica.slurm
+    # sed -i "s|sub2convert|${i}|" ${path2slurmScripts}/${i}_adni_clinica.slurm
+    # sed -i "s|adni_clinica_log|adni_${i}_clinica_log|" ${path2slurmScripts}/${i}_adni_clinica.slurm
 done
